8d3233cf46cd555d44bf988b8a2b487a9c52acbe
wellington <wellingtonsoaresferreira.jr@gmail.com>
2023-11-21 10:50:15 -0300

    instalaçao do handlebars e do nodemon

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 
 const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) =>{
     resposta.send('Ola, mundo!')
