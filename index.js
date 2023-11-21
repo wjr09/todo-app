948885e94c7ecd34abfe6f1193b0a4b2bf861f93
wellington <wellingtonsoaresferreira.jr@gmail.com>
2023-11-21 10:34:28 -0300

    primeira execuçao do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express")
 
 const app = express()
+
+app.get('/', (requisicao, resposta) =>{
+    resposta.send('Ola, mundo!')
+})
+
+app.listen(3000, () =>{
+    console.log("Servidor rodando na porta 3000!")
+}) 
